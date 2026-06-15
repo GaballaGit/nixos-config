d3bcab7a591448e831137882444892a8f3f1a24a
GaballaGit <mgaballa.dev@gmail.com>
2026-06-14 17:15:58 -0700

    allow ipv6 and ipv4 to connect to postgres

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -184,9 +184,12 @@
   services.postgresql = {
     enable = true;
     ensureDatabases = ["mydatabase"];
+    # blud thought ipv6 was allowed :sob:
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser auth-method
       local all      all    trust
+      host  all      all    ::1/128         trust
+      host  all      all    127.0.0.1/32    trust
     '';
   };
 
