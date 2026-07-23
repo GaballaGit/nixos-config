f48c5c1c73285839d4e4a4fa22aac50947fbb51f
GaballaGit <mgaballa.dev@gmail.com>
2026-07-23 02:52:37 -0700

    update hostname for mizuki

--- machines/mizuki/configuration.nix
+++ machines/mizuki/configuration.nix
@@ -30,7 +30,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "mizuki"; # Define your hostname.
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
